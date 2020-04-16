c68aff3e94e7be02ba15824f6bf78cbf82b20804
maytinhdibo <iammaytinhdibo@gmail.com>
2020-04-16 16:47:49 +0700

    explore tab layout

--- app/src/main/java/com/mtc/mindbook/LibFragment.java
+++ app/src/main/java/com/mtc/mindbook/LibFragment.java
@@ -8,14 +8,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.mtc.mindbook.explore.NearFragment;
+import com.mtc.mindbook.models.explore.SectionPageAdapter;
+import com.mtc.mindbook.explore.ShareFragment;
 
 
 public class LibFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_lib, container, false);
 
-        return inflater.inflate(R.layout.activity_lib, container, false);
+        ViewPager mViewPager = (ViewPager) rootView.findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(mViewPager);
+        return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionPageAdapter adapter = new SectionPageAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new ShareFragment(), "Share");
+        adapter.addFragment(new NearFragment(), "Near");
+        viewPager.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/mtc/mindbook/models/explore/SectionPageAdapter.java
@@ -0,0 +1,44 @@
+package com.mtc.mindbook.models.explore;
+
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionPageAdapter extends FragmentPagerAdapter {
+
+    // This array list will gonna add the fragment one after another
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    // This list of type string is for the title of the tab
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    public SectionPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+}
