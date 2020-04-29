9545ac7de279f4bd8e0da9944b36e931cb08e44c
DucHoangManh <gelous3@gmail.com>
2020-04-29 20:11:18 +0700

    save user Data + navigate

--- app/src/main/java/com/mtc/mindbook/ProfileFragment.java
+++ app/src/main/java/com/mtc/mindbook/ProfileFragment.java
@@ -1,14 +1,14 @@
 package com.mtc.mindbook;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,20 +16,42 @@ import androidx.fragment.app.Fragment;
 
 
 public class ProfileFragment extends Fragment {
-    TextView message = null;
     Button goToLoginBtn = null;
+    Button logoutBtn = null;
+    SharedPreferences sharedPrefs = null;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.activity_profile, container, false);
         goToLoginBtn = rootView.findViewById(R.id.button_go_to_login);
-        message = rootView.findViewById(R.id.profile_message);
+        logoutBtn = rootView.findViewById(R.id.btn_logout);
+        sharedPrefs = getActivity().getSharedPreferences("userDataPrefs", Context.MODE_PRIVATE);
+        if (sharedPrefs.getBoolean("isLoggedIn", false)) {
+            logoutBtn.setText("Đăng xuất");
+            Log.d("token", sharedPrefs.getString("accessToken", ""));
+        } else {
+            logoutBtn.setText("Đăng nhập");
+        }
         if (savedInstanceState != null) {
             boolean isLogin = savedInstanceState.getBoolean("isLogin");
             if (isLogin) {
                 goToLoginBtn.setVisibility(View.INVISIBLE);
             }
         }
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!sharedPrefs.getBoolean("isLoggedIn", false)) {
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivityForResult(intent, 1);
+                } else {
+                    SharedPreferences.Editor editor = sharedPrefs.edit();
+                    editor.putBoolean("isLoggedIn", false);
+                    editor.commit();
+                    logoutBtn.setText("Đăng nhập");
+                }
+            }
+        });
         goToLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,12 +65,11 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.d("dd", "onActivityResult: ");
         if (requestCode == 1){
             if (resultCode != 0) {
-                String reply = data.getStringExtra("accessToken");
-                Toast.makeText(getActivity(), reply, Toast.LENGTH_SHORT).show();
-                message.setText(reply);
                 goToLoginBtn.setVisibility(View.INVISIBLE);
+                logoutBtn.setText("Đăng xuất");
             }
         }
     }
