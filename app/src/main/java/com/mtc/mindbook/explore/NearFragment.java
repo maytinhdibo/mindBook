60177f5f23af6213f7f164af23f27adbeb038148
maytinhdibo <iammaytinhdibo@gmail.com>
2020-04-30 16:13:19 +0700

    check network location

--- app/src/main/java/com/mtc/mindbook/explore/NearFragment.java
+++ app/src/main/java/com/mtc/mindbook/explore/NearFragment.java
@@ -1,17 +1,30 @@
 package com.mtc.mindbook.explore;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mtc.mindbook.R;
+import com.mtc.mindbook.SearchActivity;
 import com.mtc.mindbook.models.BookItem;
 import com.mtc.mindbook.models.explore.NearItem;
 import com.mtc.mindbook.models.explore.RecyclerNearAdapter;
@@ -24,11 +37,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class NearFragment extends Fragment {
+public class NearFragment extends Fragment implements LocationListener {
+    private LocationManager locationManager;
+    private LinearLayout locationAlert;
+    private RecyclerView nearListView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_near, container, false);
+
+        locationAlert = rootView.findViewById(R.id.location_req);
 
         NearItem[] shareItems = {
                 new NearItem("Tôi sống một thời thơ ấu chịu ảnh hưởng hoàn toàn của các anh chị tôi, hai chị và hai anh lớn, họ ca hát bài gì thì tôi lặp lại đúng bài đó, họ ngâm nga bài thơ nào thì tôi nhớ lõm bõm mấy câu của bài đó. Bây giờ kiểm điểm lại, về các bài hát tôi nhớ nhiều, gần như trọn vẹn ca điệu và ca từ của mỗi bài, còn thơ chỉ thuộc đây đó một số câu.", "Bin Gết"
@@ -47,20 +66,92 @@ public class NearFragment extends Fragment {
                         , new BookItem(111, "https://res.cloudinary.com/fen-learning/image/upload/c_limit,w_320,h_475/infopls_images/images/HPusa5_320x475.jpg", "Quyển sách 1", "Tác Giả", (float) 1.5))
         };
 
+
         // Construct the data source
-        final List<NearItem> listShare =  new ArrayList<>(Arrays.asList(shareItems));
+        final List<NearItem> listShare = new ArrayList<>(Arrays.asList(shareItems));
         final RecyclerNearAdapter adapter = new RecyclerNearAdapter(listShare);
 
 
-        RecyclerView listView = rootView.findViewById(R.id.near_list);
+        nearListView = rootView.findViewById(R.id.near_list);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        listView.setLayoutManager(layoutManager);
+        nearListView.setLayoutManager(layoutManager);
 
-        listView.setAdapter(adapter);
+        nearListView.setAdapter(adapter);
+
+        Button openLocation = rootView.findViewById(R.id.open_location_cog);
+
+        openLocation.setOnClickListener(new View.OnClickListener() {
+                                            public void onClick(View view) {
+                                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                                startActivity(intent);
+                                            }
+                                        }
+        );
+
+
+        checkLocation(rootView, true);
+
 
         return rootView;
+    }
+
+    public void checkLocation(View rootView, boolean request) {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            locationAlert.setVisibility(View.VISIBLE);
+            nearListView.setVisibility(View.GONE);
+
+            if (request) {
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION
+                        },
+                        123);
+            }
+
+        } else {
+            locationAlert.setVisibility(View.GONE);
+            nearListView.setVisibility(View.VISIBLE);
+            Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+
+        }
+    }
+
+    private boolean firstTime = false;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (firstTime) {
+            firstTime = false;
+        } else {
+            checkLocation(getView(), false);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latT = location.getLatitude();
+        double longT = location.getLongitude();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
