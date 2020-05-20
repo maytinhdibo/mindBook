b5087a7e229d03397f0ba096ef097c737dfa6d1f
maytinhdibo <iammaytinhdibo@gmail.com>
2020-05-20 17:07:00 +0700

    update navigation and status bar

--- app/src/main/java/com/mtc/mindbook/DetailActivity.java
+++ app/src/main/java/com/mtc/mindbook/DetailActivity.java
@@ -178,8 +178,9 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        Window w = getWindow();
+//        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
 
         View view = findViewById(android.R.id.content).getRootView();
@@ -239,13 +240,13 @@ public class DetailActivity extends AppCompatActivity {
         });
 
 
-        LinearLayout bottomBar = (LinearLayout) findViewById(R.id.bottom_bar);
-        bottomBar.setLayoutParams(new LinearLayout.LayoutParams
-                (LinearLayout.LayoutParams.MATCH_PARENT
-                        , 240 + this.getNavigationBarHeight()));
-
-        View decor = getWindow().getDecorView();
-        decor.setSystemUiVisibility(0);
+//        LinearLayout bottomBar = (LinearLayout) findViewById(R.id.bottom_bar);
+//        bottomBar.setLayoutParams(new LinearLayout.LayoutParams
+//                (LinearLayout.LayoutParams.MATCH_PARENT
+//                        , 240 + this.getNavigationBarHeight()));
+//
+//        View decor = getWindow().getDecorView();
+//        decor.setSystemUiVisibility(0);
 
 
         final AppCompatActivity self = this;
